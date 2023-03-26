a54d6f1a25b17931a9d11cec1196d57ddf06517d
Ttou <1361572192@qq.com>
2023-03-26 17:01:29 +0800

    chore(custom): 更新依赖

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 const { defineLintStagedConfig } = require('@ttou/define-config')
 
 module.exports = defineLintStagedConfig({
-  '*.{ts,tsx,vue}': ['eslint --fix', 'prettier --write'],
+  '*.{ts,vue}': ['eslint --fix', 'prettier --write'],
   '*.{css,scss,vue}': ['stylelint --fix', 'prettier --write']
 })
