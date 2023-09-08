4fe22e0a63c7510617d9f1451720bf5f423bec63
WangruiJin <1361572192@qq.com>
2023-09-08 15:42:35 +0800

    feat(custom): 完善代码

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
       link: [{ rel: 'icon', type: 'image/svg+xml', href: '/vue.svg' }]
     }
   },
-  css: ['@/assets/css/main.css', '@/assets/css/theme.css'],
+  css: ['@/assets/styles/main.css'],
   devtools: {
     enabled: true
   },
