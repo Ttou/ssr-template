27b0ebe869c32b30871fe38d788dcf1b2e15c5a8
wangruijin <1361572192@qq.com>
2023-12-04 16:52:57 +0800

    chore(custom): 更新配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -13,6 +13,7 @@ module.exports = defineStylelintConfig({
     'property-no-unknown': [true, { ignoreProperties: ['/composes/'] }],
     'selector-class-pattern': null,
     'selector-pseudo-class-no-unknown': null,
-    'no-descending-specificity': null
+    'no-descending-specificity': null,
+    'import-notation': null
   }
 })
