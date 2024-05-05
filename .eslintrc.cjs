d3cd2b6dba0fdb0016efbb3a1c99c7c8899baec5
wangruijin <1361572192@qq.com>
2024-05-05 23:24:34 +0800

    build(custom): 更新配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,7 @@ module.exports = {
     'vue/attribute-hyphenation': 'off',
     'vue/multi-word-component-names': 'off',
     'vue/no-reserved-component-names': 'off',
+    'vue/v-on-event-hyphenation': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off'
