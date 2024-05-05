86071f99af7c6717e6f2a024331b70708102d879
wangruijin <1361572192@qq.com>
2024-05-05 23:02:22 +0800

    build(custom): 更新配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,9 @@ module.exports = {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'prettier/prettier': 'error',
+    'vue/attribute-hyphenation': 'off',
     'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off'
